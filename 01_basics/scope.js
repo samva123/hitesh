4b5e0cb1af50257321a7f852c0ace6df4eb21397
samva123 <149029509+samva123@users.noreply.github.com>
2024-05-17 01:11:11 +0000

    hello

---
+++ 01_basics/scope.js
@@ -0,0 +1,8 @@
+function addone( num){
+    return num+1 ;
+    // in this type of function we can use them before defining it but in below expression/function we can't use them
+}
+
+const addtwo = function(num){
+    return num+2;
+}
