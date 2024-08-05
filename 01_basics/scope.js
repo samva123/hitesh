4cadaf60f1796c13e55400ef4b0c292b516e092f
samva123 <samva123>
2024-08-05 16:26:25 +0000

    Pushed from Locofy Builder

--- 01_basics/scope.js
+++
@@ -1,8 +0,0 @@
-function addone( num){
-    return num+1 ;
-    // in this type of function we can use them before defining it but in below expression/function we can't use them
-}
-
-const addtwo = function(num){
-    return num+2;
-}
